f75ea1f883d6773e62b4cef4ee2717d739f24705
Ryan Hartman <ryan@nextlevel.com>
2019-12-27 16:10:21 -0500

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+
+// Initialize Discord Bot
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}`);
+});
+
+bot.on('message', msg => {
+    if(msg.content == 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+    }
+});
