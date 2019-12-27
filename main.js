2d7aeb3a21c4adb7af8945fedacb7879c73a8804
Ryan Hartman <rhartman1239@gmail.com>
2019-12-27 17:03:42 -0500

    Commando

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-
-// Initialize Discord Bot
-const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
-bot.login(TOKEN);
-
-bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}`);
+const Commando = require('discord.js-commando');
+const path = require('path');
+const token = process.env.TOKEN;
+const client = new Commando.Client({
+    owner: process.env.ID,
 });
 
-bot.on('message', msg => {
+client.registry
+    .registerGroups([
+    ['help', 'Helpful commands'],
+    ['other', 'Miscellaneous commands']
+    ])
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.login(token);
+client.on('ready', () => {
+    console.info(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', msg => {
     if(msg.content == 'ping') {
         msg.reply('pong');
         msg.channel.send('pong');
